f4ae7d72573e688307892c8fea006c2fcc5fe1e5
Angel Castañeda <redkazta@gmail.com>
2026-06-18 22:02:32 -0600

    chore: update social URLs and restore videos page

--- src/data/siteConfig.ts
+++ src/data/siteConfig.ts
@@ -6,13 +6,15 @@ export const TRACK_SRC = "/audio/track.mp3";
 export const KAZTA_IG_LOGO = "/brand/kazta-logo.png";
 
 export const YOUTUBE_URL = "https://www.youtube.com/@Garguttt";
+export const YOUTUBE_CHANNEL_ID = "@Garguttt";
 export const SPOTIFY_URL =
   "https://open.spotify.com/intl-es/track/4KvIxXUbDzMicaBr2opF9e?si=ece662d11f1a48c0";
 export const SPOTIFY_ARTIST_URL = "https://open.spotify.com/artist/3IWeUxh0WKXHNXJ6ahJUtX";
 export const SPOTIFY_TRACK_ID = "4KvIxXUbDzMicaBr2opF9e";
 export const SPOTIFY_EMBED_ARTIST = "https://open.spotify.com/embed/artist/3IWeUxh0WKXHNXJ6ahJUtX";
 export const SPOTIFY_EMBED_TRACK = `https://open.spotify.com/embed/track/${SPOTIFY_TRACK_ID}`;
-export const INSTAGRAM_URL = "https://www.instagram.com/young_kazta/";
+export const INSTAGRAM_URL = "https://www.instagram.com/garguttt/";
+export const TIKTOK_URL = "https://www.tiktok.com/@gargutttt";
 
 export const SITE_URL = "https://garguttieneunchingodeferia.com";
 export const SITE_TITLE = "Garguttieneunchingodeferia";
